426f6e22d7f50a30199657922a73912d19eb7a8c
Sean Wang <t113318091@ntut.edu.tw>
2025-06-10 14:39:39 +0800

    113318091_V1

--- nn_predict.py
+++ nn_predict.py
@@ -4,11 +4,13 @@ import json
 # === Activation functions ===
 def relu(x):
     # TODO: Implement the Rectified Linear Unit
-    return x
+    return np.maximum(0, x)
+
 
 def softmax(x):
-    # TODO: Implement the SoftMax function
-    return x
+    x_shifted = x - np.max(x, axis=-1, keepdims=True)
+    exp_x = np.exp(x_shifted)
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
